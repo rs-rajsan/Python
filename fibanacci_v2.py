a5e2ef0b13bc633e3e43b585dfabd38ceceec8e9
rs-rajsan <rs_rajsan@yahoo.com>
2023-12-14 22:50:09 -0500

    Printing Fibanocci in a list

---
+++ fibanacci_v2.py
@@ -0,0 +1,12 @@
+# Adding fibanocci series in a list and printing the list
+lst = [0,1,1]
+# print(lst)
+num = int(input("Enter number of Digits to print in your Fibanocci Series : "))
+
+# Starting for loop to append next value in series
+for i in range(3, num):
+  # Appending items to list
+  lst.append(lst[i-2]+lst[i-1])
+
+# Printing the Series
+print(lst)
